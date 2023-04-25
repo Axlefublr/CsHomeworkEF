e6bdf319fdd9530fc8f1af9c4609ca2e1e89b339
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-04-26 05:08:56 +0800

    add Repositories

---
+++ EFConsole/Repositories/UserRepository.cs
@@ -0,0 +1,12 @@
+namespace EFConsole.Repositories;
+
+public class UserRepository
+{
+
+    private AppContext db;
+
+    public UserRepository(AppContext db)
+    {
+        this.db = db;
+    }
+}
