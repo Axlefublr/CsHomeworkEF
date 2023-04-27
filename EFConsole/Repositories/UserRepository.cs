5fcd258a5e45e886126a1905e04dfe1ee66cd6bf
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-04-27 16:31:51 +0800

    implement GetUserById

--- EFConsole/Repositories/UserRepository.cs
+++ EFConsole/Repositories/UserRepository.cs
@@ -9,4 +9,9 @@ public class UserRepository
     {
         this.db = db;
     }
+
+    public List<User> GetUserById(int id)
+    {
+        return db.Users.Where(user => user.Id == id).Select(user => user).ToList();
+    }
 }
