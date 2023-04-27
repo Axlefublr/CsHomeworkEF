62089f780ce98ad2ae23bccc62c27bb4bf4a0a68
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-04-27 16:00:17 +0800

    add id for Book & User

--- EFConsole/Book.cs
+++ EFConsole/Book.cs
@@ -2,6 +2,7 @@ namespace EFConsole;
 
 public class Book
 {
+    public int Id { get; set; }
     public string Title { get; set; }
     public int ReleaseYear { get; set; }
 }
--- EFConsole/User.cs
+++ EFConsole/User.cs
@@ -2,6 +2,7 @@ namespace EFConsole;
 
 public class User
 {
+    public int Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
 }
