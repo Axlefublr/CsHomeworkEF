fefb379c575a09df8aa7691f7d39a33bf5229c5c
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-04-27 17:57:30 +0800

    Book: add AuthorName, Genre

--- EFConsole/Book.cs
+++ EFConsole/Book.cs
@@ -5,4 +5,6 @@ public class Book
     public int Id { get; set; }
     public string Title { get; set; }
     public int ReleaseYear { get; set; }
+    public string AuthorName { get; set; }
+    public string Genre { get; set; }
 }
